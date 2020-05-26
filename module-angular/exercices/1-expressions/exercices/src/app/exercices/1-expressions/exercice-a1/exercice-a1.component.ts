dbee7491e233b799d81b3d5720bd040a8a72d793
Jordan <48123323+JordanSOA@users.noreply.github.com>
2020-05-26 20:52:31 +0200

    Expressions-exo1

---
+++ module-angular/exercices/1-expressions/exercices/src/app/exercices/1-expressions/exercice-a1/exercice-a1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercice-a1',
+  templateUrl: './exercice-a1.component.html',
+  styleUrls: ['./exercice-a1.component.css']
+})
+export class ExerciceA1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
