0a3b97b74935fe7acc76f1781d0a69a700ea7f66
Sami Radi <sami.radi@virtuoworks.com>
2020-05-29 11:52:25 +0200

    ven. 29 mai 2020 11:52:25

---
+++ module-angular/cours/exemple02/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TotoComponent } from './toto/toto.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TotoComponent
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
